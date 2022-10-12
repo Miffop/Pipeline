bf5f3ee2c150745a6c35551b2e8ba4ababcbf7c7
Miffop <sash.kryuko2015@gmail.com>
2022-10-12 18:09:15 +0300

    AST

---
+++ Pipeline/AST/AST.fs
@@ -0,0 +1,33 @@
+﻿namespace Pipeline.AST
+
+
+type PData = obj
+type PFunc = delegate of obj->obj
+
+type PFunOrData = 
+    |Fun of PFunc
+    |Data of PData
+
+type PContex(parent:PContex option) = 
+    
+    let mutable object:obj = null
+    let defs = System.Collections.Generic.Dictionary<string,PFunOrData>()
+
+    new ()  = new PContex(None)
+    
+    member this.Parent = parent
+    member this.Object with get() = object and set v = object<-v
+    member this.Defenitions = defs
+
+    member this.Find (defname:string) = 
+         match defs.TryGetValue(defname),parent with
+         |(true,result),_->result
+         |(false,_),Some(parent)->parent.Find(defname)
+         |_->raise <| new System.Exception()
+    member this.Add = defs.Add
+
+type ICommand =
+    abstract Exec:PContex->PData
+
+type IExpression = 
+    abstract Eval:PContex->PFunOrData
