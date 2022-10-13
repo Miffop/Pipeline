e20f330a715546f6d80df7a7e1fa789a7d9cbd84
Miffop <sash.kryuko2015@gmail.com>
2022-10-13 12:59:29 +0300

    Tokens Parser 1

---
+++ Pipeline/Parser/Tokens/Token.fs
@@ -0,0 +1,61 @@
+﻿namespace Pipeline.Parser.Tokens
+
+type TokenContent(toktype:string,content:string) = 
+    member this.Type = toktype
+    member this.Content = content
+
+type Token(toktype:string,content:string,margin:int,offset:int,line:int) = 
+    new (tc:TokenContent,margin,offset,line) = new Token(tc.Type,tc.Content,margin,offset,line)
+    member this.Type = toktype
+    member this.Content = content
+    member this.Line = line
+    member this.Margin = margin //отступ всей строки в которой находиться токен
+    member this.Offset = offset //положение в строке за вычетом отступа
+    member this.TokenContent = new TokenContent(toktype,content)
+    
+    interface System.IComparable<Token> with
+        member this.CompareTo other = 
+            let marginDifference = this.Margin-other.Margin
+            let offsetDifference = this.Offset-other.Offset
+            if marginDifference<>0 then
+                marginDifference
+            else
+                offsetDifference
+
+type ITokenParser = 
+    abstract GetLength:code:string*index:int->int
+    abstract GetToken:code:string*index:int->TokenContent option
+type TokenParser(parser:ITokenParser seq) = 
+    member this.Parse (code:string) =
+        let mutable index = 0
+        let mutable line = 1
+        let mutable margin = 0
+        let mutable offset = 0
+        let mutable isMargin = true
+        let TokList = System.Collections.Generic.List<Token>()
+
+        while index<code.Length do
+            let tokOption,tokLength=
+                parser
+                |> Seq.maxBy(fun p->p.GetLength(code,index))
+                |> (fun p->p.GetToken(code,index),p.GetLength(code,index))
+            match tokOption with
+            |Some(tok)->TokList.Add(Token(tok,margin,offset,line))
+            |None->()
+            for c in code.Substring(index,tokLength) do                
+                match c with
+                |'\n'->
+                    line<-line+1
+                    margin<-0
+                    offset<-0
+                    isMargin<-true
+                |' ' when isMargin->
+                    margin<-margin+1
+                |'\t'->
+                    raise<|System.Exception("tabs are not allowed")
+                |_ ->
+                    offset<-offset+1
+                    isMargin<-false
+            index<-index+tokLength
+
+        ()
---
+++ Pipeline/Parser/Tokens/TokenParsers/WordParser.fs
@@ -0,0 +1,22 @@
+﻿namespace Pipeline.Parser.Tokens.TokenParsers
+
+open Pipeline.Parser.Tokens
+
+type WordParser() = 
+    interface ITokenParser with
+        member this.GetLength (code,index) = 
+            if System.Char.IsLetter(code.[index]) then
+                let mutable cnt = 0
+                while System.Char.IsLetterOrDigit(code.[index+cnt]) do
+                    cnt<-cnt+1
+                cnt
+            else
+                0
+        member this.GetToken (code,index) = 
+            let mutable str = ""
+            let mutable cnt = 0
+            while System.Char.IsLetterOrDigit(code.[index+cnt]) do
+                str<-str+string(code.[index+cnt])
+                cnt<-cnt+1
+            match str with
+            |s->Some<|TokenContent("Word",s)
