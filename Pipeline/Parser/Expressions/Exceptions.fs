ff697fe6fbe07ec47be7a5ed969ee59a75740ec8
Miffop <sash.kryuko2015@gmail.com>
2022-11-15 16:55:49 +0300

    Сообщения у исключений

--- Pipeline/Parser/Expressions/Exceptions.fs
+++ Pipeline/Parser/Expressions/Exceptions.fs
@@ -2,18 +2,16 @@
 
 
 type UnknownOperationExpression(name:string) = 
-    inherit System.Exception()
-    override this.Message = sprintf "неизвесная операция %s" name
-    
+    inherit System.Exception(sprintf "неизвесная операция %s" name)
+
 type OperationIsNotBinary(name:string) = 
-    inherit System.Exception()
-    override this.Message = sprintf "операция %s операция не является бинарной" name
+    inherit System.Exception(sprintf "операция %s операция не является бинарной" name)
+
 type OperationIsNotUnary(name:string) = 
-    inherit System.Exception()
-    override this.Message = sprintf "операция %s не является унарной" name
+    inherit System.Exception(sprintf "операция %s не является унарной" name)
+
 type OperationIsNotUnaryLeft(name:string) = 
-    inherit System.Exception()
-    override this.Message = sprintf "унарная операция %s не принимает операнд слева" name
+    inherit System.Exception(sprintf "унарная операция %s не принимает операнд слева" name)
+
 type OperationIsNotUnaryRight(name:string) = 
-    inherit System.Exception()
-    override this.Message = sprintf "унарная операция %s не принимает операнд справа" name
+    inherit System.Exception(sprintf "унарная операция %s не принимает операнд справа" name)
